02c91290e2191181ba99d804f372765dc0b1b375
xinfangfang <550212808@qq.com>
2022-06-20 23:16:09 +0800

    fix

---
+++ docs/.vuepress/components/BottomData.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="FirstBottomPage">
+            Copyright © 2022-present 心方方
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.FirstBottomPage {
+    border-top: 1px solid #eaecef;
+    padding-top: 30px;
+    margin-top: 40px;
+    text-align: center;
+}
+</style>
